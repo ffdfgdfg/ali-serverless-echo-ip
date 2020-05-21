df4a98105a095c65dfcf6667a33d6a9b7607e314
ffdfgdfg <heyingtingaaa@gmail.com>
2020-05-21 21:04:02 +0800

    init

---
+++ index.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import logging
+# HELLO_WORLD = b'Hello world!\n'
+
+# To enable the initializer feature (https://help.aliyun.com/document_detail/158208.html)
+# please implement the initializer function as below：
+# def initializer(context):
+#    logger = logging.getLogger()  
+#    logger.info('initializing')
+
+
+def handler(environ, start_response):
+    # context = environ['fc.context']
+    # request_uri = environ['fc.request_uri']
+    # for k, v in environ.items():
+    #   print('k:{} v:{}'.format(k, v))
+    #   if k.startswith('HTTP_'):
+    #     # process custom request headers
+    #     pass
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    print(environ['REMOTE_ADDR'])
+    return [environ['REMOTE_ADDR'].encode('utf-8')]
